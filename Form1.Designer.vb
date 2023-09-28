220b74eb129e97012e90e2338974990229e263cd
ranneytech <c21-1094-551@pangasinan.uphsl.edu.ph>
2023-09-28 21:13:00 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(218, 59)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(175, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(218, 105)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(175, 23)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(140, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(253, 24)
+        Label1.TabIndex = 2
+        Label1.Text = "Letter to Color Conversion"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(82, 59)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(130, 24)
+        Label2.TabIndex = 3
+        Label2.Text = "Enter Letter :"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(132, 104)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(80, 24)
+        Label3.TabIndex = 4
+        Label3.Text = "Result :"
+        ' 
+        ' Button1
+        ' 
+        Button1.Font = New Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.Location = New Point(250, 149)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(102, 33)
+        Button1.TabIndex = 5
+        Button1.Text = "Convert"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(636, 302)
+        Controls.Add(Button1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim userInput As String = TextBox1.Text.ToLower()
+
+        Dim colorResult As String
+
+        Select Case userInput
+            Case "r", "R"
+                colorResult = "Red"
+            Case "b", "B"
+                colorResult = "Blue"
+            Case "g", "G"
+                colorResult = "Green"
+            Case Else
+                colorResult = "Unknown color"
+        End Select
+        TextBox2.Text = colorResult
+    End Sub
+End Class
